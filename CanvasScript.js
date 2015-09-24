d5b8b434e8fe477644b17ab4e18f26117c3d36a9
Veronica Gunn <vfgunn@utexas.edu>
2015-09-24 16:26:53 -0500

    Continued refactoring and got listeners for file uploading made.

--- CanvasScript.js
+++ CanvasScript.js
@@ -2,14 +2,49 @@ var setUp = function() {
 	setUpFileUpload();
 }
 
+var createCanvasAndDrawImage = function() {
+	alert("Called create canvas!");
+}
+
+var loadImage = function(fileReader) {
+	var image = new Image();
+	image.src = fileReader.result;
+	$(image).load(createCanvasAndDrawImage);
+}
+
 /*
  * This function validates that a given file is a valid image
  * and then creates a new canvas and draws the image on the
  * canvas.
  * TODO(vfgunn): Finish implementation
  */
-var handleUpload = function() {
+var handleUpload = function(fileInput) {
+	var file = fileInput.files[0];
+	if (!isValidImage(file)) {
+		return;
+	}
 	var fileReader = new FileReader();
+
+	fileReader.readAsDataURL(file);
+
+	// Remove an already existing listener if one is set
+	$(fileReader).off("load");
+	$(fileReader).load(function() {
+		loadImage(fileReader);
+	});
+}
+
+/*
+ * Returns true if a given file is an image.
+ * If a given file is not an image, alerts
+ * the user and prompts for an image
+ */
+var isValidImage = function(file) {
+	if (!file.type.match(/image.*/)) {
+		alert("Please upload an image file!");
+		return false;
+	}
+	return true;
 }
 
 /*
@@ -22,7 +57,9 @@ var setUpFileUpload = function() {
     input.id = "fileUploadBox";
     $("#pageContent").append(input);
 
-    $(input).change(handleUpload);
+    $(input).change(function() {
+    	handleUpload(input);
+    });
 }
 
 window.onload = setUp;
